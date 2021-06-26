84dacfb0f031b293ecd8203ef7045bf839d6ad08
shibinglanya <shibinglanya@yeah.net>
2021-06-27 00:55:14 +0800

    1

--- resources/desktop/scripts/detect-clipboard.sh
+++ resources/desktop/scripts/detect-clipboard.sh
@@ -15,8 +15,7 @@ do
   if [ "$Var_Display0_Clipboard" != "$Display0_Clipboard" ]; then
     Display0_Clipboard=$Var_Display0_Clipboard
     Display1_Clipboard=$Var_Display0_Clipboard
-    echo $Display0_Clipboard | DISPLAY=:1 xsel --input --clipboard
-
+    echo "$Display0_Clipboard" | DISPLAY=:1 xsel --input --clipboard
     echo "1: $Display0_Clipboard"
     continue
   fi
@@ -25,7 +24,7 @@ do
   if [ "$Var_Display1_Clipboard" != "$Display1_Clipboard" ]; then
     Display0_Clipboard=$Var_Display1_Clipboard
     Display1_Clipboard=$Var_Display1_Clipboard
-    echo $Display1_Clipboard | DISPLAY=:0 xsel --input --clipboard
+    echo "$Display1_Clipboard" | DISPLAY=:0 xsel --input --clipboard
     echo "2: $Display0_Clipboard"
     continue
   fi
