f6c78ecb0167e095d55c762cc2f229b02a16be76
shibinglanya <shibinglanya@yeah.net>
2021-06-30 11:55:39 +0800

    1

--- resources/desktop/dwm/config.h
+++ resources/desktop/dwm/config.h
@@ -111,7 +111,8 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
-static const char *termcmd[]  = { "alacritty", "-e", "tmux", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
+
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "/home/shibinglanya/.config/scripts/tmux.start-st-terminal.sh", NULL };
 
