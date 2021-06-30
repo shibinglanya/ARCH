b9c307b2aaf1129a6e693fa603bbb73dc609c5a3
shibinglanya <shibinglanya@yeah.net>
2021-06-30 20:29:51 +0800

    1

--- resources/desktop/scripts/startplasma-x11.sh
+++ resources/desktop/scripts/startplasma-x11.sh
@@ -10,8 +10,8 @@ done
 sleep 1
 DISPLAY=:1 startplasma-x11 &
 
-Display0_Clipboard="$(DISPLAY=:0 xsel --output --clipboard)"
-Display1_Clipboard="$(DISPLAY=:1 xsel --output --clipboard)"
+Display0_Clipboard=`DISPLAY=:0 xsel --output --clipboard`
+Display1_Clipboard=`DISPLAY=:1 xsel --output --clipboard`
 
 while true
 do
@@ -25,9 +25,9 @@ do
   fi
 
   if [ "$Var_Display0_Clipboard" != "$Display0_Clipboard" ]; then
-    Display0_Clipboard=$Var_Display0_Clipboard
-    Display1_Clipboard=$Var_Display0_Clipboard
-    echo -n "$Display0_Clipboard" | DISPLAY=:0 xsel --input --clipboard
+    Display0_Clipboard="$Var_Display0_Clipboard"
+    Display1_Clipboard="$Var_Display0_Clipboard"
+    #echo -n "$Display0_Clipboard" | DISPLAY=:0 xsel --input --clipboard
     echo -n "$Display0_Clipboard" | DISPLAY=:1 xsel --input --clipboard
     continue
   fi
@@ -35,10 +35,10 @@ do
   Var_Display1_Clipboard="$(DISPLAY=:1 xsel --output --clipboard)"
   [ "$Var_Display1_Clipboard" == "" ] && continue
   if [ "$Var_Display1_Clipboard" != "$Display1_Clipboard" ]; then
-    Display0_Clipboard=$Var_Display1_Clipboard
-    Display1_Clipboard=$Var_Display1_Clipboard
+    Display0_Clipboard="$Var_Display1_Clipboard"
+    Display1_Clipboard="$Var_Display1_Clipboard"
     echo -n "$Display1_Clipboard" | DISPLAY=:0 xsel --input --clipboard
-    echo -n "$Display1_Clipboard" | DISPLAY=:1 xsel --input --clipboard
+    #echo -n "$Display1_Clipboard" | DISPLAY=:1 xsel --input --clipboard
     continue
   fi
 done
