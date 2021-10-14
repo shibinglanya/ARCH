cadc78b0ad31e3f68c5c503baa86ebc33e0dfe78
shibinglanya <shibinglanya@yeah.net>
2021-10-14 09:59:42 +0800

    dwm:config.h

--- resources/desktop/dwm/config.h
+++ resources/desktop/dwm/config.h
@@ -92,8 +92,6 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "🌠",      tile },    /* first entry is default */
-	{ "N",      NULL },    /* no layout function means floating behavior */
-	{ "M",      monocle },
 };
 
 /* key definitions */
@@ -112,7 +110,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *termcmd[]  = { "alacritty", "-e", "tmux", NULL };
 
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "/home/shibinglanya/.config/scripts/tmux.start-st-terminal.sh", NULL };
